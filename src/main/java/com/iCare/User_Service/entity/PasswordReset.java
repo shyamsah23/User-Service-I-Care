d12727d4feddd1b293d73485a5490d245ad2e343
shyamsah23 <92447257+shyamsah23@users.noreply.github.com>
2025-11-08 12:46:04 +0530

    Entity Created

---
+++ src/main/java/com/iCare/User_Service/entity/PasswordReset.java
@@ -0,0 +1,62 @@
+package com.iCare.User_Service.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "PassWord_Reset")
+public class PasswordReset {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String token;
+    private LocalDateTime expireAt;
+    @OneToOne
+    private User user;
+
+    public PasswordReset() {
+    }
+
+    public PasswordReset(Long id, String token, LocalDateTime expireAt, User user) {
+        this.id = id;
+        this.token = token;
+        this.expireAt = expireAt;
+        this.user = user;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public LocalDateTime getExpireAt() {
+        return expireAt;
+    }
+
+    public void setExpireAt(LocalDateTime expireAt) {
+        this.expireAt = expireAt;
+    }
+    
+}
